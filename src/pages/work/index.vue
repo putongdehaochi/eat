0ef6de2da971ea2c0aa30ef8114d65214c751be2
shijian23 <418554980@qq.com>
2022-11-06 14:07:54 +0800

    feat:工作台修改

--- src/pages/work/index.vue
+++ src/pages/work/index.vue
@@ -1,6 +1,6 @@
 <template>
   <view :class="styles.container">
-    <view :class="styles.title">待办事项</view>
+    <view :class="styles.title">忌口</view>
     <nut-cell :class="styles.list">
       <nut-list :height="50" :listData="count" @scroll-bottom="handleScroll">
         <template v-slot="{ item }">
@@ -21,13 +21,10 @@ import { List } from "./constant.ts";
 const count = ref(List);
 
 const handleScroll = () => {
-  let arr = new Array(100).fill(0);
-  const len = count.value.length;
-  count.value = count.value.concat(arr.map((item, index) => len + index + 1));
+
 };
 
 onMounted(() => {
-  // count.value = count.value.map((item, index) => index + 1);
 });
 </script>
 
